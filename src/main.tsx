b2f2b6c56ffc7569e0510b3f6b35dca5dc7859c2
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-13 16:23:22 -0300

    Testing components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ScrollDirection from "./components/ScrollDirection";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ScrollDirection>
+      <App />
+    </ScrollDirection>
   </React.StrictMode>
 );
