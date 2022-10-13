4b44b9ab331929e1005930facfe56cf17cbc8998
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-13 18:33:30 -0300

    Adapted code to the new functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import useProximity from "./hooks/useProximity";
 function App() {
   const ref = useRef<HTMLDivElement>(null);
 
-  const [element, setElement] = useState<HTMLDivElement | null>(null);
-
-  const proximity = useProximity(element);
-
-  useLayoutEffect(() => {
-    if (ref.current) setElement(ref.current);
-  }, []);
+  const proximity = useProximity(ref);
 
   return <div ref={ref}>{proximity}</div>;
 }
--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -4,13 +4,7 @@ import useProximity from "../hooks/useProximity";
 const Render: FC<any> = ({ children }) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  const [element, setElement] = useState<HTMLDivElement | null>(null);
-
-  const proximity = useProximity(element);
-
-  useLayoutEffect(() => {
-    if (ref.current) setElement(ref.current);
-  }, []);
+  const proximity = useProximity(ref);
 
   return (
     <div ref={ref}>{proximity >= 0 && proximity <= 2 ? children : null}</div>
