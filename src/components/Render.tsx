aaa5dbf24b2d48826d851d211a3792857b9dc970
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-13 18:52:14 -0300

    Now rrenders accepts styles

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useLayoutEffect, useRef, useState } from "react";
 import useProximity from "../hooks/useProximity";
 
-const Render: FC<any> = ({ children }) => {
+const Render: FC<any> = ({ children, style }) => {
   const ref = useRef<HTMLDivElement>(null);
 
   const proximity = useProximity(ref);
 
   return (
-    <div ref={ref}>{proximity >= 0 && proximity <= 2 ? children : null}</div>
+    <div style={style} ref={ref}>
+      {proximity >= 0 && proximity <= 2 ? children : null}
+    </div>
   );
 };
 
