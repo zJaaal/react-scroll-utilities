e8d22e8fc87b7eef4cab2d0f03820a3fe9aea326
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-14 14:53:10 -0300

    Added use cases test for Render Component

---
+++ src/__test__/Render.test.tsx
@@ -0,0 +1,37 @@
+import { cleanup, fireEvent, render } from "@testing-library/react";
+import { afterEach, beforeAll, describe, expect, it } from "vitest";
+import Render from "../components/Render";
+import getBoundingClientRectMock from "./mock/getBoundingClientRectMock";
+
+describe("Render Component", () => {
+  afterEach(cleanup);
+
+  beforeAll(() => {
+    HTMLElement.prototype.getBoundingClientRect = getBoundingClientRectMock;
+  });
+
+  it("should render on a proximity between 0 and 2", () => {
+    const RenderTestComponent = render(<Render>Hi i'm being rendered</Render>);
+    fireEvent.scroll(window, {
+      target: { scrollY: window.innerHeight / 2 },
+    });
+    expect(
+      RenderTestComponent.getByText("Hi i'm being rendered"),
+      "Component should render when the proximity is between 0 and 2"
+    ).toBeTruthy();
+  }),
+    it("should not render out of a proximity between 0 and 2", () => {
+      const RenderTestComponent = render(
+        <Render>Hi i'm not being rendered</Render>
+      );
+
+      fireEvent.scroll(window, {
+        target: { scrollY: window.innerHeight * 2 },
+      });
+
+      expect(
+        RenderTestComponent.queryByText("Hi i'm not being rendered"),
+        "Component should be out of bounds of proximity acceptance"
+      ).toBeFalsy();
+    });
+});
