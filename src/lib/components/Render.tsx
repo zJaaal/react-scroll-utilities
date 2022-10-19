2921706952aae2f004ed6052087563b1824ac30f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-19 18:19:08 -0300

    Now Render component works as expected with new imlpementation

--- src/lib/components/Render.tsx
+++ src/lib/components/Render.tsx
@@ -1,14 +1,18 @@
-import React, { FC, useLayoutEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
 import useProximity from "../hooks/useProximity";
 
 const Render: FC<any> = ({ children, style }) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  const proximity = useProximity(ref);
+  const { x, y } = useProximity(ref);
+
+  useEffect(() => {
+    console.log(x, y);
+  }, [x, y]);
 
   return (
     <div style={{ height: "inherit", ...style }} ref={ref}>
-      {proximity >= 0 && proximity <= 2 ? children : null}
+      {y >= 0 && y <= 2 && x >= 0 && x <= 3 ? children : null}
     </div>
   );
 };
