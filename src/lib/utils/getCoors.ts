744839b2bec76d9d1df8fdc941eb6c5b23d5b2b2
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-19 18:17:25 -0300

    Now context returns y and x coors

---
+++ src/lib/utils/getCoors.ts
@@ -0,0 +1,17 @@
+import React from "react";
+import { Coors } from "../types";
+
+const getCoors = (): Coors => {
+  return {
+    x:
+      typeof window.scrollX == "undefined"
+        ? document.documentElement.scrollLeft
+        : window.scrollX,
+    y:
+      typeof window.scrollY == "undefined"
+        ? document.documentElement.scrollTop
+        : window.scrollY,
+  };
+};
+
+export default getCoors;
