a91c97b11526395767fe47e857eec1feeb13369f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-20 13:08:27 -0300

    fixed test for Render component

--- src/__test__/Render.test.tsx
+++ src/__test__/Render.test.tsx
@@ -11,7 +11,7 @@ describe("Render Component", () => {
     HTMLElement.prototype.getBoundingClientRect = getBoundingClientRectMock;
   });
 
-  it("should render on a proximity between 0 and 2", () => {
+  it("should render on a proximity between 0 and 2 for Y", () => {
     const RenderTestComponent = render(
       <ScrollWatcher>
         <Render>Hi i'm being rendered</Render>
@@ -20,12 +20,17 @@ describe("Render Component", () => {
     fireEvent.scroll(window, {
       target: { scrollY: window.innerHeight / 2 },
     });
+
+    fireEvent.scroll(window, {
+      target: { scrollX: window.innerWidth / 2 },
+    });
+
     expect(
       RenderTestComponent.getByText("Hi i'm being rendered"),
-      "Component should render when the proximity is between 0 and 2"
+      "Component should render when the proximity is between 0 and 2 for Y and between 0 and 3 for X"
     ).toBeTruthy();
   }),
-    it("should not render out of a proximity between 0 and 2", () => {
+    it("should not render out of a proximity between 0 and 2 for Y and out of between 0 and 3 for X", () => {
       const RenderTestComponent = render(
         <ScrollWatcher>
           <Render>Hi i'm not being rendered</Render>
@@ -36,9 +41,13 @@ describe("Render Component", () => {
         target: { scrollY: window.innerHeight * 2 },
       });
 
+      fireEvent.scroll(window, {
+        target: { scrollX: window.innerWidth * 2 },
+      });
+
       expect(
         RenderTestComponent.queryByText("Hi i'm not being rendered"),
-        "Component should be out of bounds of proximity acceptance"
+        "Component should be out of bounds of proximity acceptance for Y and and out of between 0 and 3 for X"
       ).toBeFalsy();
     });
 });
