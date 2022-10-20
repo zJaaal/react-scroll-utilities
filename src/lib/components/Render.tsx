154636afebd4a00e9b5290b45d40f76cf1411cf3
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-20 14:37:42 -0300

    small fixes
    v0.3.1

--- src/lib/components/Render.tsx
+++ src/lib/components/Render.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from "react";
+import React, { FC, useRef } from "react";
 import useProximity from "../hooks/useProximity";
 
 const Render: FC<any> = ({ children, style }) => {
