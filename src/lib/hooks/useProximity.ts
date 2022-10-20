054559a2c247a7a6b450aff63b602ac8d32514f4
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-20 13:26:52 -0300

    Added validation and fixed documentation on useProximity

--- src/lib/hooks/useProximity.ts
+++ src/lib/hooks/useProximity.ts
@@ -8,14 +8,15 @@ import React, {
 import ScrollContext from "../context/ScrollContext";
 import { Coors } from "../types";
 import getProximity from "../utils/getProximity";
+import validateRef from "../utils/validateRef";
 import validateScrollValue from "../utils/validateScrollValue";
 
 /**
  * @description This hooks returns the proximity of the viewport from the component.
  * @param ref A reference to the HTMLElement
- * @returns A float value that represents the proximity to the component. The closer to 1 the closer is the middle of the screen from the component.
+ * @returns An object with 2 properties: x amd y. Values that represents the proximity to the component. The closer to 1 the closer is the middle of the screen from the component.
  */
-const useProximity = (ref: RefObject<any>) => {
+const useProximity = (ref: RefObject<HTMLElement>) => {
   const initialState: Coors = {
     x: 0,
     y: 0,
@@ -28,8 +29,12 @@ const useProximity = (ref: RefObject<any>) => {
   const [element, setElement] = useState<HTMLElement | null>(null);
 
   useLayoutEffect(() => {
-    if (ref) setElement(ref.current);
+    if (ref.current) {
+      validateRef(ref.current);
+      setElement(ref.current);
+    }
   }, []);
+
   useEffect(() => validateScrollValue(scrollState), []);
 
   useEffect(() => {
