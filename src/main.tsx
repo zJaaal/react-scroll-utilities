91c39a4923d40af85599c636b60329fe61a1bd43
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-21 15:29:51 -0300

    just messing around with the visual test files

--- src/main.tsx
+++ src/main.tsx
@@ -8,25 +8,24 @@ import BackgroundChange from "./test-components/BackgroundChange";
 import Circle from "./test-components/Circle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ScrollWatcher>
-      <h1>Scroll!</h1>
-      <App />
-      <BackgroundChange>
-        <Render
-          style={{
-            height: "inherit",
-            position: "relative",
-            transform: "translate(80%, 50%)",
-          }}
-        >
-          <h3 className="test-h3">I'm spinning</h3>
-        </Render>
-
-        <Circle>
-          <h3 className="test-h3">I'm spinning</h3>
-        </Circle>
-      </BackgroundChange>
-    </ScrollWatcher>
-  </React.StrictMode>
+  <ScrollWatcher>
+    <h1>Scroll!</h1>
+    <App />
+    <BackgroundChange>
+      <Render
+        style={{
+          height: "inherit",
+          position: "relative",
+          transform: "translate(80%, 50%)",
+        }}
+      >
+        <h3 className="test-h3">I'm spinning</h3>
+      </Render>
+    </BackgroundChange>
+    <Circle>
+      <h3 className="test-h3">I'm spinning</h3>
+    </Circle>
+  </ScrollWatcher>
+  // </React.StrictMode>
+  //         <React.StrictMode>
 );
