b63f1f75ed070b81c48a42076d022b6e52cd02d3
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-24 17:13:55 -0300

    fixed typo

--- src/lib/hooks/useProximity.ts
+++ src/lib/hooks/useProximity.ts
@@ -8,7 +8,7 @@ import validateScrollValue from "../utils/validateScrollValue";
 /**
  * @description This hooks returns the proximity of the viewport from the component.
  * @param ref A reference to the HTMLElement
- * @returns An object with 2 properties: x amd y. Values that represents the proximity to the component. The closer to 1 the closer is the middle of the screen from the component.
+ * @returns An object with 2 properties: x and y. Values that represents the proximity to the component. The closer to 1 the closer is the middle of the screen from the component.
  */
 const useProximity = (ref: RefObject<HTMLElement>) => {
   const initialState: Coors = {
