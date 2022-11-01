2a4b27b09d29cb1662cbb8e3ea68b1986e51954e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-01 18:17:42 -0300

    now clamp works perfectly

--- src/lib/utils/validations/clamp.ts
+++ src/lib/utils/validations/clamp.ts
@@ -1,5 +1,7 @@
-const clamp = (min: number, max: number, value: number) => {
-  return Math.max(min, Math.min(value, max));
+const clamp = (firstPivot: number, secondPivot: number, value: number) => {
+  if (firstPivot < secondPivot)
+    return Math.max(firstPivot, Math.min(value, secondPivot));
+  else return Math.max(secondPivot, Math.min(value, firstPivot));
 };
 
 export default clamp;
