afb61e7b79b76770f474ba3df456939e6b356f7e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-01 18:10:56 -0300

    need to make this calculation smarter

--- src/lib/utils/calculations/calculateSteps.ts
+++ src/lib/utils/calculations/calculateSteps.ts
@@ -5,12 +5,18 @@ const calculateSteps = (
 ): number[] => {
   let steps: number[] = [];
 
+  //need to make this smarter
   startColor.forEach((color, i) => {
     if (color == endColor[i]) return steps.push(0);
 
     if (color > endColor[i])
-      steps.push((element.clientHeight / (color - endColor[i])) * -1);
-    else steps.push(element.clientHeight / (endColor[i] - color));
+      steps.push(
+        Math.floor((endColor[i] - color) / element.clientHeight - 0.9999)
+      );
+    else
+      steps.push(
+        Math.ceil((endColor[i] - color) / element.clientHeight + 0.9999)
+      );
   });
 
   return steps;
