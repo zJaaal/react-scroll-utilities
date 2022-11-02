e5ea181e9b3ec2b67d6cffa511d09a64a330b992
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-02 17:10:00 -0300

    Update getProximity.ts
    Now if the value is more than zero, the component is up the center and if is less than zero is under the center

--- src/lib/utils/calculations/getProximity.ts
+++ src/lib/utils/calculations/getProximity.ts
@@ -6,8 +6,8 @@ const getProximity = (element: HTMLElement): ProximityState => {
 
   return {
     //This calculations return the coordinates of the center of the component
-    x: x + width / 2 - window.innerWidth / 2,
-    y: y + height / 2 - window.innerHeight / 2,
+    x: (x + width / 2 - window.innerWidth / 2) * -1,
+    y: (y + height / 2 - window.innerHeight / 2) * -1,
     onSight:
       !(bottom < 0 || top - window.innerHeight >= 0) &&
       !(right < 0 || left - window.innerWidth >= 0),
