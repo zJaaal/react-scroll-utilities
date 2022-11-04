638263c214c7965dfedd931b78c700d442a25b0f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-04 14:39:52 -0300

    small fixes

--- src/lib/utils/calculations/linearValue.ts
+++ src/lib/utils/calculations/linearValue.ts
@@ -1,7 +1,7 @@
-const linearValue = (slope: number, y1: number, x: number, x1: number) => {
+const linearValue = (m: number, y1: number, x: number, x1: number) => {
   //this is the result of finding y on the linear equation
   // (y - y1) = m(x - x1)
-  const y = slope * x - slope * x1 + y1;
+  const y = m * x - m * x1 + y1;
 
   return y;
 };
