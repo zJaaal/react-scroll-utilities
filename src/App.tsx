41424a017465c2ae6f8a24e8a64502cab4978ebc
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-17 13:37:33 -0300

    Added Footer and Dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Direction from "./site/components/direction/Direction";
+import Dynamic from "./site/components/dynamic/Dynamic";
+import Footer from "./site/components/footer/Footer";
 import Header from "./site/components/header/Header";
 import Proximity from "./site/components/proximity/Proximity";
 
@@ -8,6 +10,8 @@ function App() {
       <Header />
       <Proximity />
       <Direction />
+      <Dynamic />
+      <Footer />
     </div>
   );
 }
---
+++ src/site/components/dynamic/Dynamic.tsx
@@ -0,0 +1,68 @@
+import React, { useRef } from "react";
+import { Rectangle, useDynamicColor } from "../../../lib";
+
+const Dynamic = () => {
+  const ref = useRef(null);
+  const exampleRef = useRef(null);
+
+  const backgroundColor = useDynamicColor({
+    startColor: [255, 255, 255],
+    endColor: [159, 212, 163],
+    elementRef: ref,
+  });
+  const color = useDynamicColor({
+    startColor: [159, 212, 163],
+    endColor: [255, 255, 255],
+    elementRef: ref,
+  });
+
+  const exampleColor = useDynamicColor({
+    startColor: [239, 112, 155],
+    endColor: [16, 55, 131],
+    elementRef: exampleRef,
+  });
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor,
+        paddingTop: "30px",
+      }}
+      ref={ref}
+    >
+      <Rectangle
+        backgroundColor={backgroundColor}
+        color={color}
+        stroke={5}
+        width={window.innerWidth / 2}
+        height={100}
+        speed={1.2}
+      >
+        <h1 style={{ color: color, fontSize: "40px" }}>useDynamicColor</h1>
+      </Rectangle>
+      <h2 style={{ marginTop: "20px", color, width: "50%" }}>
+        Basically, this hook creates an animation of colors that goes from an{" "}
+        <span style={{ color: "#ef709b" }}>start color</span> to an{" "}
+        <span style={{ color: "#103783" }}>end color.</span>
+      </h2>
+      <h2
+        style={{ marginTop: "40px", color: exampleColor, width: "50%" }}
+        ref={exampleRef}
+      >
+        You've been seeing examples of this through all the demo. I love this
+        hook to be honest.
+      </h2>
+      <h2
+        style={{ marginTop: "40px", color: exampleColor, width: "50%" }}
+        ref={exampleRef}
+      >
+        PD: Just need this space for you to appreciate the animation. ;)
+      </h2>
+    </div>
+  );
+};
+
+export default Dynamic;
