39237b86ed9e6865fcdd946d59ac64b0a1362a8e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-18 16:07:28 -0300

    Create useLinearValue.ts
    Now you can calculate values for animations using a linear function

---
+++ src/lib/hooks/useLinearValue.ts
@@ -0,0 +1,80 @@
+import React, { RefObject, useLayoutEffect, useRef } from "react";
+import clamp from "../utils/calculations/clamp";
+import getLinearValue from "../utils/calculations/getLinearValue";
+import { LinearValue } from "../utils/calculations/types";
+import useProximity from "./useProximity";
+
+const useLinearValue = (
+  startValue: number,
+  endValue: number,
+  elementRef: RefObject<HTMLElement>,
+  anchor: "top" | "middle" | "bottom" = "middle"
+) => {
+  const { onSight, y } = useProximity(elementRef);
+  const value = useRef(startValue);
+
+  useLayoutEffect(() => {
+    const { top, bottom } =
+      anchor != "middle"
+        ? elementRef.current!.getBoundingClientRect()
+        : { top: 0, bottom: 0 };
+
+    if (onSight) {
+      switch (anchor) {
+        case "middle": {
+          const linearValue: LinearValue = {
+            x1:
+              Math.max(elementRef.current!.clientHeight, window.innerHeight) /
+              -2,
+            x2:
+              Math.max(elementRef.current!.clientHeight, window.innerHeight) /
+              2,
+            y1: startValue,
+            y2: endValue,
+            position: y,
+          };
+          value.current = getLinearValue(linearValue);
+          break;
+        }
+        case "top": {
+          const linearValue: LinearValue = {
+            x1: 0,
+            x2: elementRef.current!.clientHeight,
+            y1: startValue,
+            y2: endValue,
+            position: Math.abs(top),
+          };
+          value.current = getLinearValue(linearValue);
+          break;
+        }
+        case "bottom": {
+          if (
+            bottom - window.innerHeight - elementRef.current!.clientHeight ==
+            0
+          )
+            value.current = startValue;
+          if (
+            bottom - window.innerHeight - elementRef.current!.clientHeight ==
+            elementRef.current!.clientHeight
+          )
+            value.current = endValue;
+
+          const linearValue: LinearValue = {
+            x1: 0,
+            x2: elementRef.current!.clientHeight,
+            y1: startValue,
+            y2: endValue,
+            position: Math.abs(
+              bottom - window.innerHeight - elementRef.current!.clientHeight
+            ),
+          };
+          value.current = getLinearValue(linearValue);
+          break;
+        }
+      }
+    }
+  }, [y]);
+  return clamp(startValue, endValue, value.current);
+};
+
+export default useLinearValue;
