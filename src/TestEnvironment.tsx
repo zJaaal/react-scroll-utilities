ecc07d2851d9f8fbd156eef12f59dcbc5c72aae3
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-25 19:27:23 -0300

    visual testing

--- src/TestEnvironment.tsx
+++ src/TestEnvironment.tsx
@@ -4,11 +4,30 @@ import { LinearValueOptions } from "./lib/types";
 
 const TestEnvironment = () => {
   const ref = useRef(null);
+  const delayRef1 = useRef(null);
+  const delayRef2 = useRef(null);
+  const delayRef3 = useRef(null);
 
   const options: LinearValueOptions = {
-    anchor: "bottom",
+    anchor: "middle",
     delay: 0,
     duration: 100,
+  };
+
+  const optionsDelay1: LinearValueOptions = {
+    anchor: "middle",
+    delay: 20,
+    duration: 100,
+  };
+  const optionsDelay2: LinearValueOptions = {
+    anchor: "middle",
+    delay: 50,
+    duration: 80,
+  };
+  const optionsDelay3: LinearValueOptions = {
+    anchor: "middle",
+    delay: 80,
+    duration: 60,
   };
 
   const color = useDynamicColor({
@@ -19,14 +38,14 @@ const TestEnvironment = () => {
   });
 
   const height = useLinearValue({
-    startValue: 100,
-    endValue: 500,
+    startValue: 10,
+    endValue: 100,
     elementRef: ref,
     options,
   });
   const width = useLinearValue({
-    startValue: 100,
-    endValue: 500,
+    startValue: 10,
+    endValue: 100,
     elementRef: ref,
     options,
   });
@@ -36,31 +55,176 @@ const TestEnvironment = () => {
     elementRef: ref,
     options,
   });
+  const delayColor1 = useDynamicColor({
+    startColor: [67, 206, 162],
+    endColor: [24, 90, 157],
+    elementRef: delayRef1,
+    options: optionsDelay1,
+  });
+
+  const delayHeight1 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef1,
+    options: optionsDelay1,
+  });
+  const delayWidth1 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef1,
+    options: optionsDelay1,
+  });
+  const delayDeg1 = useLinearValue({
+    startValue: 0,
+    endValue: 90,
+    elementRef: delayRef1,
+    options: optionsDelay1,
+  });
+  const delayColor2 = useDynamicColor({
+    startColor: [67, 206, 162],
+    endColor: [24, 90, 157],
+    elementRef: delayRef2,
+    options: optionsDelay2,
+  });
+
+  const delayHeight2 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef2,
+    options: optionsDelay2,
+  });
+  const delayWidth2 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef2,
+    options: optionsDelay2,
+  });
+  const delayDeg2 = useLinearValue({
+    startValue: 0,
+    endValue: 90,
+    elementRef: delayRef2,
+    options: optionsDelay2,
+  });
+  const delayColor3 = useDynamicColor({
+    startColor: [67, 206, 162],
+    endColor: [24, 90, 157],
+    elementRef: delayRef3,
+    options: optionsDelay3,
+  });
+
+  const delayHeight3 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef3,
+    options: optionsDelay3,
+  });
+  const delayWidth3 = useLinearValue({
+    startValue: 10,
+    endValue: 100,
+    elementRef: delayRef3,
+    options: optionsDelay3,
+  });
+  const delayDeg3 = useLinearValue({
+    startValue: 0,
+    endValue: 90,
+    elementRef: delayRef3,
+    options: optionsDelay3,
+  });
 
   return (
     <div>
       <div
         style={{
-          height: 4000,
+          height: "4000px",
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
+          gap: 20,
         }}
       >
         <div
-          style={{
-            height,
-            width,
-            backgroundColor: color,
-            color: "black",
-            transform: `rotate(${deg}deg)`,
-            borderRadius: "10px",
-          }}
           ref={ref}
-        ></div>
-      </div>
-      <div style={{ height: 400 }}>
-        <Circle radius="40vh" stroke={2}></Circle>
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 20,
+          }}
+        >
+          <div
+            style={{
+              height,
+              width,
+              backgroundColor: color,
+              color: "black",
+              transform: `rotate(${deg}deg)`,
+              borderRadius: "10px",
+            }}
+          ></div>
+          <div
+            style={{
+              height,
+              width,
+              backgroundColor: color,
+              color: "black",
+              transform: `rotate(${deg}deg)`,
+              borderRadius: "10px",
+            }}
+          ></div>
+          <div
+            style={{
+              height,
+              width,
+              backgroundColor: color,
+              color: "black",
+              transform: `rotate(${deg}deg)`,
+              borderRadius: "10px",
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 20,
+          }}
+        >
+          <div
+            style={{
+              height: delayHeight1,
+              width: delayWidth1,
+              backgroundColor: delayColor1,
+              color: "black",
+              transform: `rotate(${delayDeg1}deg)`,
+              borderRadius: "10px",
+            }}
+            ref={delayRef1}
+          ></div>
+          <div
+            style={{
+              height: delayHeight2,
+              width: delayWidth2,
+              backgroundColor: delayColor2,
+              color: "black",
+              transform: `rotate(${delayDeg2}deg)`,
+              borderRadius: "10px",
+            }}
+            ref={delayRef2}
+          ></div>
+          <div
+            style={{
+              height: delayHeight3,
+              width: delayWidth3,
+              backgroundColor: delayColor3,
+              color: "black",
+              transform: `rotate(${delayDeg3}deg)`,
+              borderRadius: "10px",
+            }}
+            ref={delayRef3}
+          ></div>
+        </div>
       </div>
     </div>
   );
