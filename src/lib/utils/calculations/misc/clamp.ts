491372233c502ef37c4220bc54a7ea02ae277c5d
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-02 14:13:53 -0300

    return value to a proximity of 2 decimals

--- src/lib/utils/calculations/misc/clamp.ts
+++ src/lib/utils/calculations/misc/clamp.ts
@@ -1,7 +1,7 @@
 const clamp = (firstPivot: number, secondPivot: number, value: number) => {
   if (firstPivot < secondPivot)
-    return Math.max(firstPivot, Math.min(value, secondPivot));
-  else return Math.max(secondPivot, Math.min(value, firstPivot));
+    return Number(Math.max(firstPivot, Math.min(value, secondPivot)).toFixed(2));
+  else return Number(Math.max(secondPivot, Math.min(value, firstPivot)).toFixed(2));
 };
 
 export default clamp;
