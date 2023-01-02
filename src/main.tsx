c315bba4bfb551a330fa482824bb26665265c381
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-02 14:16:06 -0300

    visual testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ScrollWatcher } from "./lib";
-import TestEnvironment from "./TestEnvironment";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ScrollWatcher } from './lib';
+import TestEnvironment from './TestEnvironment';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ScrollWatcher>
-    <App />
-    {/* <TestEnvironment /> */}
+    {/* <App /> */}
+    <TestEnvironment />
   </ScrollWatcher>
 );
